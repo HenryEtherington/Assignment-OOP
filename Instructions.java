3944d4c90ea077f7c230c8a07f51863208ca6f8f
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-11-08 22:24:31 +0000

    Add files via upload

---
+++ Instructions.java
@@ -0,0 +1,12 @@
+
+    public interface Instructions   {
+
+       //Controls
+
+        String cText = "Controls:" +
+                "\nUP Arrow to move up" +
+                "\nLEFT Arrow to move left" +
+                "\nRIGHT Arrow to move right" +
+                "\nDOWN Arrow to move down";
+   }
+
