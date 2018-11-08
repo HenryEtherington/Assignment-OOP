c9e54d02f47860b9d6a96bc63d4028a75c147fe0
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-11-08 22:24:25 +0000

    Add files via upload

--- History.java
+++ History.java
@@ -1,12 +1,12 @@
-import javax.swing.*;
 
-public class History {
-	
-	 public static void main (String args[]) {
-	 
- //https://en.wikipedia.org/wiki/Pong
-    	 JOptionPane.showMessageDialog(null,"Pong is one of the earliest arcade video games. It is a table tennis sports game featuring simple two-dimensional graphics. The game was originally manufactured by Atari, which released it in 1972. Allan Alcorn created Pong as a training exercise assigned to him by Atari co-founder Nolan Bushnell. Bushnell based the idea on an electronic ping-pong game included in the Magnavox Odyssey, which later resulted in a lawsuit against Atari. Surprised by the quality of Alcorn's work, Bushnell and Atari co-founder Ted Dabney decided to manufacture the game.","History of pong",JOptionPane.INFORMATION_MESSAGE);  
-   
-	 }
-    
+public interface History   {
+
+       //History
+
+	String hText = "Pong is one of the earliest arcade video games. It is a table tennis sports game featuring simple two-dimensional graphics." +
+			"\nThe game was originally manufactured by Atari, which released it in 1972. Allan Alcorn created Pong as a training exercise" +
+			"\nassigned to him by Atari co-founder Nolan Bushnell. Bushnell based the idea on an electronic ping-pong game included in" +
+			"\nthe Magnavox Odyssey, which later resulted in a lawsuit against Atari. Surprised by the quality of Alcorn's work, Bushnell" +
+			"\nand Atari co-founder Ted Dabney decided to manufacture the game.";
 }
+
