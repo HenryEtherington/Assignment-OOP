59737b60ccd6172570512d9cda56d686e3b2b9e9
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-11-09 13:13:59 +0000

    Add files via upload

---
+++ Paddle2.java
@@ -0,0 +1,60 @@
+import javax.sql.CommonDataSource;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*; 
+
+
+   public class Paddle2 extends JPanel implements ActionListener, PongUI {
+   	
+   	
+   	
+   	public void paddle2(Graphics g, int xVel, int yVel, int width, int height) {
+   		
+   	g.drawRect(xVel, yVel, width, height);
+   		
+   	}
+   	
+    int x, y;
+
+    //Timer timer;
+   //lock area gui
+   //if off screen error!
+//Reference Me
+
+   Paddle2() {
+//Initial paddle position
+        x = 480;
+        
+        //timer = new Timer(10, this);
+  }
+
+   	      public void actionPerformed(ActionEvent e) {
+ //paddle moves up or down;
+ 
+ 
+        repaint();
+    }
+   		
+   	   public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        paddle2(g, x, 200, 1, 88);
+
+    }
+   		
+   		public static void main(String[] args) {
+   		
+   		Paddle2 paddle2 = new Paddle2();
+   		JFrame frame = new JFrame("Paddle2");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      //  Paddle1 moving = new Paddle2();
+        frame.add(paddle2);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        //moving.timer.start();
+  
+
+   		}
+    }
+   
