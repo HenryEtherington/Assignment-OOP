0ccc8707cf4e7460552d2b754b2c98d6ba437b23
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-11-11 14:27:13 +0000

    Add files via upload

--- PongUI.java
+++ PongUI.java
@@ -1,25 +1,29 @@
 import javax.swing.JFrame;
 
-    public class PongUI   {
+    public class PongUI  extends Ball {
     	
         //https://www.youtube.com/watch?v=5okwKe6L1Tk
     
-    	public static void main (String[] args) {}
+    	public static void main (String[] args) {
+    		
+    		}
    			
   //Ball extends Paddle1 which extends Paddle2 which extends JPanel and both implement ActionListner
    			
    							
    	 public void gui() {
          	
-        Ball ball = new Ball();
+        //Ball ball = new Ball();
+        Paddle2 pad = new Paddle2();
    		JFrame frame = new JFrame("Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(ball);
+        frame.add(pad);
+        //frame.add(ball);
         frame.setSize(1200,900);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        ball.timer.start();   
-
+        //ball.timer.start();  
+	
       }
    	
    	
