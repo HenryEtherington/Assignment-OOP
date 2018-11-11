3c7e110e3ad14e352a502e6c2ee4eeaa3817e5cd
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-11-11 21:30:44 +0000

    Add files via upload

--- Paddle1.java
+++ Paddle1.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.*; 
 
   //Abstarct method
-   public abstract class Paddle1 extends Ball  implements ActionListener {
+   public abstract class Paddle1 extends JPanel implements ActionListener {
    	
    	
    	
