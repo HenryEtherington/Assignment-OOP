05352f63727a094b13c80cf8fd4ec24abf4a0203
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-11-11 21:31:07 +0000

    Add files via upload

--- Ball.java
+++ Ball.java
@@ -4,18 +4,18 @@ import java.awt.event.*;
 
 public class Ball extends JPanel implements ActionListener{
 
+ 
+
     int x, y;
 
-    Timer timer;
    
 
     Ball() {
-//Initial Ball Position
+ 
+ //Initial Ball Position
         x = 595;
         y = 445;
-   
-        	Timer t = new Timer (10,this);  
-        		t.start();
+        Timer t = new Timer (10,this);     
   }
 
    
@@ -31,12 +31,11 @@ public class Ball extends JPanel implements ActionListener{
     public void paintComponent(Graphics g) {
 
         super.paintComponent(g);
-
-        g.fillOval(x, y, 10, 10);
-
-    }
-
+        g.fillOval(x, y, 10, 10);        
    
+    }
+    
+
 
     //if (ball radius + distance from paddle radius)  less then or equal to (radius paddle) //Collision occurs
     //else // Collision does not occur
