81b11a2e63bdc8079bbadfa0b2b06bdeaf377326
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-11-11 14:27:19 +0000

    Add files via upload

--- Paddle2.java
+++ Paddle2.java
@@ -1,20 +1,22 @@
+   		
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*; 
 
 
 
-   public class Paddle2 extends JPanel implements ActionListener {
+
+   public class Paddle2 extends JPanel implements ActionListener, KeyListener {   	
    	
    	
-   	
-   	public void paddle2(Graphics g, int xVel, int yVel, int width, int height) {
+   	Timer t = new Timer (5,this);
+    int x = 1100, y = 300 , velX = 0, velY = 0;
+    
+     	public void paddle2(Graphics g, int xVel, int yVel, int width, int height) {
    		
    	g.drawRect(xVel, yVel, width, height);
    		
    	}
-   	
-    int x, y;
 
     //Timer timer;
    //lock area gui
@@ -23,15 +25,19 @@ import java.awt.event.*;
 
    Paddle2() {
 //Initial paddle position
-        x = 1100;
-        y = 300;
+
+        t.start();
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
         //timer = new Timer(10, this);
   }
 
    	      public void actionPerformed(ActionEvent e) {
  //paddle moves up or down;
  
- 
+        x = x += velX;
+        y = y += velY;
         repaint();
     }
 
@@ -39,7 +45,6 @@ import java.awt.event.*;
    	   public void paintComponent(Graphics g) {
  
         super.paintComponent(g);
-
         paddle2(g, x, y, 1, 200);
 
     }
@@ -56,7 +61,29 @@ import java.awt.event.*;
         //moving.timer.start();
   
    		} */
-    }
-   
- 
- 		
+   		
+   		public void keyPressed (KeyEvent evt)
+   		{
+   			int i = evt.getKeyCode();
+   			
+   			if(i == KeyEvent.VK_UP)
+   				
+   			{
+   				velY = -2;
+   				velX = 0;
+   			}
+   			
+   			else if(i == KeyEvent.VK_DOWN)
+   				
+   			{
+   			    velY = 2;
+   				velX = 0;
+   			}
+   		}
+   		
+   		public void keyTyped(KeyEvent evt){}
+   		public void keyReleased(KeyEvent evt){}
+   		
+   		
+   		
+   } 		
