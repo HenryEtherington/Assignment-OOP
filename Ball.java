f270bfdefe760e5624e092d8e91508488ac0792f
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-11-13 22:00:28 +0000

    Add files via upload

--- Ball.java
+++ Ball.java
@@ -8,14 +8,16 @@ public class Ball extends JPanel implements ActionListener{
 
     int x, y;
 
-   
+   Timer timer;
 
     Ball() {
  
  //Initial Ball Position
         x = 595;
         y = 445;
-        Timer t = new Timer (10,this);     
+        timer = new Timer (10,this);     
+    
+       		 
   }
 
    
@@ -36,11 +38,21 @@ public class Ball extends JPanel implements ActionListener{
     }
     
     
-  
-        
+
+
+   public static void main(String[] args) {
+
+
+
+      
+
+    }
+    
+    
 //Double Buffer image;
 
     //if (ball radius + distance from paddle radius)  less then or equal to (radius paddle) //Collision occurs
     //else // Collision does not occur
  
 }
+
