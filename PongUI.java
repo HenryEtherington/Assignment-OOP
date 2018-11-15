ec18b1c6bd8f3637ba474cc6651d0ca1f8d8e8b8
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2018-11-15 13:08:37 +0000

    Add files via upload

--- PongUI.java
+++ PongUI.java
@@ -1,34 +1,31 @@
 import javax.swing.JFrame;
 import java.util.*;
+public  class PongUI  extends JFrame {
 
-    public  class PongUI  extends JFrame {
-    	
-        //How to use multiple class in java
-        //https://www.youtube.com/watch?v=5okwKe6L1Tk
-    
-		
-  //Ball extends Paddle1 which extends Paddle2 which extends JPanel and both implement ActionListner
+    //How to use multiple class in java
+    //https://www.youtube.com/watch?v=5okwKe6L1Tk
 
-		
-   	 public void gui() {
-        
-       
-        //  Timer timer;
-        //Ball b = new Ball();
-        // timer = new Timer(10,this);
-        //timer.start();	
-       // UserPad p = new UserPad();
-   		JFrame frame = new JFrame("Game");
+
+    //Ball extends Paddle1 which extends Paddle2 which extends JPanel and both implement ActionListner
+
+
+    public void gui() {
+
+
+
+         //timer = new Timer(10,this);
+        //timer.start();
+        UserPad p = new UserPad();
+        JFrame frame = new JFrame("Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       // frame.add(p);
-        //frame.add(b);
+       // Ball b = new Ball();
+       frame.add(p);
+      //  frame.add(b);
         frame.setSize(1200,900);
         frame.setLocationRelativeTo(null);
-        frame.setVisible(true);    
+        frame.setVisible(true);
         //timer.start();
-        //Ball b = new Ball();
-        //frame.add(b);
-        //b.timer.start();
-	
-      }	
+     // b.timer.start();
+
     }
+}
